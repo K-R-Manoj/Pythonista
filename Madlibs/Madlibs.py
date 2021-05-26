6879b5259fe99fa32c70954306a9a1753e332014
K-R-Manoj <manojgowdakr28@gmail.com>
2021-05-26 17:36:16 +0530

    initial commit

---
+++ Madlibs/Madlibs.py
@@ -0,0 +1,9 @@
+# String Concatenation (How to put strings together)
+# Suppose we want to create a string that says "Subscribe to ___________"
+Youtuber = "Manoj"
+
+# Types of Cancatenation
+# 1.
+print("Subscribe to "+Youtuber)
+print("Subscribe to {}".format(Youtuber))
+print(f"Subscribe to {Youtuber}")
